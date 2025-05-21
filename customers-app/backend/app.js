e81603a3eb3dee6e49bc2c5f4af3ad4d71222a10
Celsocag <celsoalbugo@gmail.com>
2025-05-21 19:46:37 -0300

    Import routing in the app

--- customers-app/backend/app.js
+++ customers-app/backend/app.js
@@ -1,14 +1,25 @@
 const express = require('express');
 
 const app = express();
+const cors = require('cors');
 
 const PORT = 3000;
 
+//enable cors
+app.use(cors());
+//enable json parser
+app.use(express.json);
+
+// route the customer api
+const customerRoutes = require('./routes/customers')
+//use the route
+app.use('/api/customers', customerRoutes)
+
 app.get('/', (req, res) => {
     res.send("Welcome to my API")
-})
+});
 
 app.listen(PORT, (err) => {
     !err ? console.log("Server is successfulll") : 
     console.log("An error occurred: ", err)
-})
+});
