31b3a09ef05576cea90308a3917b8bd467bfd2cd
Celsocag <celsoalbugo@gmail.com>
2025-06-14 23:48:12 -0300

    adding bootstrap and Material

--- customers-app/frontend/src/app/components/customer-list/customer-list.component.ts
+++ customers-app/frontend/src/app/components/customer-list/customer-list.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {  RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-customer-list',
-  imports: [],
+  imports: [
+    MatButtonModule,
+    RouterModule
+  ],
   templateUrl: './customer-list.component.html',
   styleUrl: './customer-list.component.scss'
 })
